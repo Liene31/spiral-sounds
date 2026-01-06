d545a564b164af4ae7bdc0f881cf28d4739cb844
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-01-06 22:10:55 +0100

    project setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.static("public"));
+
+app
+  .listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.log("Failed to start server:", err);
+  });
